43683390e2af8b76b23f32737a61933170d0bad0
dongjun <sdj799@naver.com>
2023-06-26 16:49:37 +0900

    # 13-3. 목록 화면 처리중...

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Join from './component/user/Join';
 import Login from './component/user/Login';
 import { Route, Routes } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
     <>
--- src/component/todo/TodoTemplate.js
+++ src/component/todo/TodoTemplate.js
@@ -2,12 +2,29 @@ import React, { useEffect, useState} from 'react'
 import TodoHeader from './TodoHeader';
 import TodoMain from './TodoMain';
 import TodoInput from './TodoInput';
+import { useNavigate } from 'react-router-dom';
+import { Spinner } from 'reactstrap';
 
 import './scss/TodoTemplate.scss'
 
 import { API_BASE_URL as BASE, TODO } from '../../config/host-config';
+import { getLoginUserInfo } from '../../utill/login-utils';
 
 const TodoTemplate = () => {
+
+  //로딩 상태값 관리
+  const [loading, setLoading] = useState(true);
+
+  const redirection = useNavigate();
+
+  // 로그인 인증 토큰 얻어오기
+  const {token} = getLoginUserInfo();
+
+  //요청 헤더 설정
+  const requestHeader = {
+    'content-type' : 'application/json',
+    'Authorization' : 'Bearer ' + token
+  }
 
   // 서버에 할일 목록(json)을 요청(fetch)해서 받아와야 함.
   const API_BASE_URL = BASE + TODO;
@@ -45,7 +62,7 @@ const TodoTemplate = () => {
 
     fetch(API_BASE_URL, {
       method : 'POST',
-      headers : {'content-type' : 'application/json'},
+      headers : requestHeader,
       body : JSON.stringify(newTodo)
     })
       .then(res => res.json())
@@ -62,7 +79,8 @@ const TodoTemplate = () => {
     //주어진 배열의 값들을 순회하여 조건에 맞는 요소들만 모아서
     //새로운 배열로 리턴해 주는 함수.
     fetch(`${API_BASE_URL}/${id}`, {
-      method : 'DELETE'
+      method : 'DELETE', 
+      headers : requestHeader
     })
     .then(res => res.json())
     .then(json => {
@@ -86,7 +104,7 @@ const TodoTemplate = () => {
 
     fetch(API_BASE_URL, {
       method : 'PUT',
-      headers : {'content-type' : 'application/json'},
+      headers : requestHeader,
       body: JSON.stringify({
         'done' : !done,
         'id' : id        
@@ -98,31 +116,63 @@ const TodoTemplate = () => {
     // setTodos(todos.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo));
   }
 
-  //체크가 안된 할 일의 개수 카운트 하기
+  //체크가 안 된 할 일의 개수 카운트 하기
   const countRestTodo = () => todos.filter(todo => !todo.done).length;
 
   useEffect(() => {
+    
     //페이지가 렌더링 됨과 동시에 할 일 목록을 요청해서 뿌려 주겠습니다.
-    fetch(API_BASE_URL)
-      .then(res => res.json())
-      .then(json => {
-        console.log(json.todos);
+    fetch(API_BASE_URL, {
+      method: 'GET',
+      headers: requestHeader
+    })
+    .then(res => {
+      if(res.status === 200) return res.json();
+      else if(res.status === 403) {
+        alert('로그인이 필요한 서비스 입니다.');
+        redirection("/login");
+        return;
+      } else {
+        alert('관리자에게 문의하세요!');
+      }
+      return;
+    })
+    .then(json => {
+      console.log(json.todos);
 
-        //fetch를 통해 받아온 데이터를 상태 변수에 할당.
-        setTodos(json.todos);
-      });
+      //fetch를 통해 받아온 데이터를 상태 변수에 할당.
+      setTodos(json.todos);
+
+      //로딩 완료 처리
+      setLoading(false);
+    });
+
   }, []);
 
-
-  return (
-    <div className='TodoTemplate'>
-        <TodoHeader count={countRestTodo}/>
+  const loadEndedPage = (
+    <>
+      <TodoHeader count={countRestTodo}/>
         <TodoMain
           todoList={todos}
           remove={removeTodo}
           check={checkTodo}
-          />
-        <TodoInput addTodo={addTodo}/>    
+        />
+      <TodoInput addTodo={addTodo}/>
+    </>
+  );
+
+  //로딩 중일 때 보여줄 컴포넌트
+  const loadingPage = (
+    <div className='loading'>
+      <Spinner color='danger'>
+        loading...
+      </Spinner>
+    </div>
+  );
+
+  return (
+    <div className='TodoTemplate'>
+      { loading ? loadingPage : loadEndedPage }    
     </div>
   );
 }
