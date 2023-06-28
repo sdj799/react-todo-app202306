3c3bfc30a57fcdb4f8f69f0b24ff8894eca22161
dongjun <sdj799@naver.com>
2023-06-28 14:49:08 +0900

    # 16. 프로필 이미지 처리중...

--- src/component/user/Join.js
+++ src/component/user/Join.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, useRef } from 'react'
 import {Button, Container, Grid,
     TextField, Typography, Link} from "@mui/material";
 
@@ -7,10 +7,13 @@ import { useNavigate } from 'react-router-dom';
 import { API_BASE_URL as BASE, USER } from '../../config/host-config';
 import AuthContext from '../../utill/AuthContext';
 import CustomSnackBar from '../layout/CustomSnackBar';
-
+import './Join.scss';
 
 const Join = () => {
     
+    //useRef로 태그 참조하기
+    const $fileTag = useRef();
+
     //리다이렉트 사용하기
     const redirection = useNavigate();
     const { isLoggedIn } = useContext(AuthContext);
@@ -221,8 +224,24 @@ const Join = () => {
             inputVal: 'pass',
             msg,
             flag
-        })
-    }
+        });
+    };
+
+    //이미지 파일 상태변수
+    const [imgFile, setImgFile] = useState(null);
+
+    //이미지 파일을 선택했을 때 썸네일 뿌리기
+    const showThumbnailHandler = e => {
+        //첨부된 파일 정보
+        const file = $fileTag.current.files[0];
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onloadend = () => {
+            setImgFile(reader.result);
+        }
+    };
 
     //4개의 입력칸이 모두 검증에 통과했는지 여부를 검사
     const isValid = () => {
@@ -235,10 +254,22 @@ const Join = () => {
 
     //회원 가입 처리 서버 요청
     const fetchSignUpPost = () => {
+
+        // JSON을 Blob타입으로 변경 후 FormData에 넣기
+        const userJsonBlob = new Blob(
+            [JSON.stringify(userValue)],
+            {type: 'application/json'}
+        );
+
+        //이미지파일과 회원정보 JSON을 하나로 묶어야 함
+        //FormData 객체를 활용해서.
+        const userFormData = new FormData();
+        userFormData.append('user', userJsonBlob);
+        userFormData.append('profileImage', $fileTag.current.files[0]);
+
         fetch(API_BASE_URL, {
             method: 'POST',
-            headers: {'content-type' : 'application/json'},
-            body: JSON.stringify(userValue)
+            body: userFormData
         })
         .then(res => {
             if(res.status === 200) {
@@ -277,6 +308,28 @@ const Join = () => {
                                 계정 생성
                             </Typography>
                         </Grid>
+
+                        <Grid item xs={12}>
+                            <div className="thumbnail-box" onClick={() => $fileTag.current.click()}>
+                                <img
+                                    // src={require("../../assets/img/image-add.png")}
+                                    // src={imgFile ? imgFile : require("../../assets/img/image-add.png")}
+                                    src={imgFile || require("../../assets/img/image-add.png")}
+                                    alt="profile"
+
+                                />
+                            </div>
+                            <label className='signup-img-label' htmlFor='profile-img'>프로필 이미지 추가</label>
+                            <input
+                                id='profile-img'
+                                type='file'
+                                style={{display: 'none'}}
+                                accept='image/*'
+                                ref={$fileTag}
+                                onChange={showThumbnailHandler}
+                            />
+                        </Grid>
+
                         <Grid item xs={12}>
                             <TextField
                                 autoComplete="fname"
