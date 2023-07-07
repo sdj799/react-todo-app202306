b2c308bcc4e7411dedc15d2aa10f53b17d894ae5
dongjun <sdj799@naver.com>
2023-07-07 14:36:02 +0900

    host-config 수정

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -6,8 +6,9 @@ let backEndHostName; // 백엔드 서버 호스트 이름
 
 if(clientHostName === 'localhost') { //개발 중
     backEndHostName = 'http://localhost:8181';
-} else if(clientHostName === 'spring.com') { //배포해서 서비스 중
-    backEndHostName = 'http://api.spring.com';
+} else if(clientHostName === 'tramovel.com') { //배포해서 서비스 중
+    // backEndHostName = 'http://api.spring.com';
+    backEndHostName = 'http://3.37.112.85';
 }
 
 export const API_BASE_URL = backEndHostName;
